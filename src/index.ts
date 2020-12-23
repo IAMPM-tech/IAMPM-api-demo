5a59fbdc35db4407494ebd5e24c4487f144dd1ff
IAMPM-tech <75525866+IAMPM-tech@users.noreply.github.com>
2020-12-23 16:15:59 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+var mongoose = require(‘mongoose’);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/iampm-tm-api-demo');
+
 import express, { Express } from 'express';
 import cors from 'cors';
 import passport from 'passport';
